38b12c64b178a6569b36465be37690bfc6aec4ef
Roomay <Roomaywoo@gmail.com>
2021-03-04 13:16:05 +0800

    Lab 1 Finished

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,11 +1,23 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @Roomay YOUR NAME HERE
  */
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
+     *	A leap year is either:
+	 *	divisible by 400 or
+	 *	divisible by 4 and not by 100.
      */
+    public static boolean isLeapYear(int year){
+    	if(year % 4 !=0){
+    		return false;
+    	}
+    	else if(year % 100 ==0 && year % 400 !=0)
+    		return false;
+    	return true;
+    }
+    
     private static void checkLeapYear(int year) {
         if (isLeapYear(year)) {
             System.out.printf("%d is a leap year.\n", year);
