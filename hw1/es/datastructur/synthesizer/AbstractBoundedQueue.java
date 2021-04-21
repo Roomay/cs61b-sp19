8da6d124113460af5fcf5529fdf0bb6c242c05bd
Roomay <Roomaywoo@gmail.com>
2021-04-21 23:14:45 +0800

    HW1 AbstractBoundedQueue added

---
+++ hw1/es/datastructur/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,12 @@
+package es.datastructur.synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
+    protected int fillCount;
+    protected int capacity;
+    public int capacity() {
+        return capacity;
+    }
+    public int fillCount() {
+        return fillCount;
+    }
+}
--- hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -7,7 +7,7 @@ import java.util.Iterator;
 // Make sure to add the override tag for all overridden methods
 // Make sure to make this class implement BoundedQueue<T>
 
-public class ArrayRingBuffer<T> implements BoundedQueue<T> {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> implements BoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
