82a6c5507d865b77ce6dff2fabcbb1141cef4771
Roomay <Roomaywoo@gmail.com>
2021-04-23 22:31:54 +0800

    Lab 6 Finished

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,35 +1,42 @@
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    //  - Add instance variables?
+    int[] verticesAffiliation;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        verticesAffiliation = new int[n];
+        for (int i = 0; i < n; i++) {
+            verticesAffiliation[i] = -1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex >= verticesAffiliation.length) {
+            throw new IllegalArgumentException();
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return (-verticesAffiliation[find(v1)]);
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return verticesAffiliation[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        validate(v1);
+        validate(v2);
+        return (find(v1) == find(v2));
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +45,30 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        validate(v1);
+        validate(v2);
+        if (!connected(v1, v2)) {
+            if (sizeOf(v1) <= sizeOf(v2)) {
+                verticesAffiliation[find(v2)] += verticesAffiliation[find(v1)];
+                verticesAffiliation[find(v1)] = find(v2);
+            } else {
+                verticesAffiliation[find(v1)] += verticesAffiliation[find(v2)];
+                verticesAffiliation[find(v2)] = find(v1);
+            }
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        validate(vertex);
+        int curVertex = vertex;
+        while (verticesAffiliation[curVertex] >= 0) {
+            curVertex = verticesAffiliation[curVertex];
+        }
+        if (verticesAffiliation[vertex] >= 0) {
+            verticesAffiliation[vertex] = curVertex;
+        }
+        return curVertex;
     }
-
 }
