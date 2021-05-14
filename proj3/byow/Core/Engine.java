808e1adb402d958dc0f80631677c351c331331e8
Roomay <80018257+Roomay@users.noreply.github.com>
2021-05-14 17:50:19 +0800

    proj3 addRooms() method works.

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -1,13 +1,47 @@
 package byow.Core;
 
+import byow.InputDemo.InputSource;
+import byow.InputDemo.StringInputDevice;
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+import edu.princeton.cs.algs4.QuickUnionUF;
+
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Random;
 
 public class Engine {
     TERenderer ter = new TERenderer();
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
+    public static final int RECTANGLE_BASE_SIZE = 4;
+
+    /* Tables and Sets storing the rooms and their connection details*/
+    private HashMap<Position, RectangleSize> roomsSize;
+    private HashMap<Integer, Position> roomsNo;
+    private QuickUnionUF roomsUnionSet;
+    private KDTree<Position> vertices;
+
+
+    class Position {
+        int x;
+        int y;
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    class RectangleSize {
+        int height;
+        int width;
+        RectangleSize(int h, int w) {
+            height = h;
+            width = w;
+        }
+    }
 
     /**
      * Method used for exploring a fresh world. This method should handle all inputs,
@@ -45,8 +79,133 @@ public class Engine {
         //
         // See proj3.byow.InputDemo for a demo of how you can make a nice clean interface
         // that works for many different input types.
+        if (input.charAt(0) != 'N' && input.charAt(0) != 'n') {
+            throw new InputMismatchException();
+        }
 
-        TETile[][] finalWorldFrame = null;
+        InputSource str = new StringInputDevice(input);
+        str.getNextKey();
+        char c;
+        StringBuilder buffer = new StringBuilder();
+        while (str.possibleNextInput()) {
+            c = str.getNextKey();
+            if (c >= '0' && c <= '9') {
+                buffer.append(c);
+            } else {
+                break;
+            }
+        }
+
+        long seed = Long.parseLong(buffer.toString());
+
+        TETile[][] finalWorldFrame = new TETile[WIDTH][HEIGHT];
+        tilesInit(finalWorldFrame);
+        generateWorldBySeed(finalWorldFrame, seed);
+
+        // Codes below process the "wsad" operations.
+
         return finalWorldFrame;
+    }
+
+    private void tilesInit(TETile[][] tiles) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        for (int x = 0; x < width; x += 1) {
+            for (int y = 0; y < height; y += 1) {
+                tiles[x][y] = Tileset.NOTHING;
+            }
+        }
+    }
+
+    private void generateWorldBySeed(TETile[][] tiles, long seed) {
+        int height = tiles[0].length;
+        int width = tiles.length;
+        Random generator = new Random(seed);
+        int numOfRooms = 20 + generator.nextInt(20);
+        addRooms(tiles, numOfRooms, generator);
+        //addHallways(tiles, generator);
+    }
+
+    private void addRooms(TETile[][] tiles, int nums, Random generator) {
+        int width = tiles[0].length;
+        int height = tiles.length;
+        roomsSize = new HashMap<>(nums);
+        roomsNo = new HashMap<>(nums);
+        roomsUnionSet = new QuickUnionUF(nums + 1);
+        vertices = new KDTree<>();
+        for (int i = 1; i <= nums; i++) {
+            Position leftLowerCorner;
+            do {
+                int posX = generator.nextInt(WIDTH - 2);
+                int posY = generator.nextInt(HEIGHT - 2);
+                leftLowerCorner = new Position(posX, posY);
+            }while (isCollided(leftLowerCorner, 1, 1));
+
+            int rectWidth = 1 + generator.nextInt(RECTANGLE_BASE_SIZE + (nums - i) / 10);
+            int rectHeight = 1 + generator.nextInt(RECTANGLE_BASE_SIZE + (nums - i) / 10);
+            addSingleRoom(tiles, leftLowerCorner, rectHeight, rectWidth, generator);
+            roomsNo.put(nums, leftLowerCorner);
+        }
+    }
+
+    private void addSingleRoom(TETile[][] tiles, Position p, int height, int width, Random generator) {
+        while (isCollided(p, height, width) && !(height == 1 && width == 1)) { // Shrink until not collided.
+            if (height > 1 && width > 1) {
+                if (generator.nextBoolean()) {
+                    height--;
+                } else {
+                    width--;
+                }
+            } else if (height == 1 && width > 1) {
+                width--;
+            } else if (height > 1 && width == 1) {
+                height--;
+            }
+        }
+
+        /* Wall tiles*/
+        int wallTopHeight = p.y + height + 2; // Excluded
+        int wallRightWidth = p.x + width + 2; // Excluded
+        int leftBar = p.x;
+        int rightBar = wallRightWidth - 1; //
+        int bottomBar = p.y;
+        int topBar = wallTopHeight - 1; //
+        for (int i = p.y; i < wallTopHeight; i++) {
+            tiles[leftBar][i] = Tileset.WALL;
+            tiles[rightBar][i] = Tileset.WALL;
+        }
+        for (int i = p.x; i < wallRightWidth; i++) {
+            tiles[i][bottomBar] = Tileset.WALL;
+            tiles[i][topBar] = Tileset.WALL;
+        }
+
+        /* Floor tiles*/
+        for (int i = bottomBar + 1; i < topBar; i++) {
+            for (int j = leftBar + 1; j < rightBar; j++) {
+                tiles[j][i] = Tileset.FLOOR;
+            }
+        }
+
+        /* Update KDTree for collision check.*/
+        vertices.put(p, leftBar, bottomBar);
+        vertices.put(new Position(leftBar, topBar), leftBar, topBar);
+        vertices.put(new Position(rightBar, topBar), rightBar, topBar);
+        vertices.put(new Position(rightBar, bottomBar), rightBar, bottomBar);
+        /* Update Room's size for hallway generation.*/
+        roomsSize.put(p, new RectangleSize(height, width)); // Put the final size into the HashMap.
+        //System.out.println("base position: " + p.x + ", " + p.y + "-- size : width " + width + ", height " + height); // test
+    }
+
+    private boolean isCollided(Position p, int height, int width) {
+        return (p.x + width + 1 >= WIDTH || p.y + height + 1 >= HEIGHT || vertices.barrierInRange(p.x, p.y, height, width, roomsSize));
+    }
+
+    public static void main(String[] args) {
+        Engine e = new Engine();
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+        TETile[][] testWorld = e.interactWithInputString("n123");
+
+        ter.renderFrame(testWorld);
     }
 }
---
+++ proj3/byow/Core/KDTree.java
@@ -0,0 +1,119 @@
+package byow.Core;
+
+
+import java.util.HashSet;
+
+import java.util.Map;
+import java.util.Stack;
+
+public class KDTree<K> {
+    private kdtNode<K> root;
+    private static final int COORDINATE_X = 0;
+    private static final int COORDINATE_Y = 1;
+
+    private kdtNode<K> put(K point, int x, int y, kdtNode<K> node, int coordinate) {
+        if (node == null) {
+            return new kdtNode<>(point, x, y, coordinate);
+        } else {
+            int pointKey = x;
+            int nodeKey = node.getX();
+            if (coordinate == COORDINATE_Y) {
+                pointKey = y;
+                nodeKey = node.getY();
+            }
+
+            /* Left Child is bound with a left / down point, right child a right / up one.*/
+            if (pointKey - nodeKey < 0) {
+                node.leftChild = put(point, x, y, node.leftChild, 1 - coordinate);
+            } else {
+                node.rightChild = put(point, x, y, node.rightChild, 1 - coordinate);
+            }
+        }
+        return node;
+    }
+
+    private class kdtNode<K> {
+        K key;
+        int posX;
+        int posY;
+        int splitDirection;
+        kdtNode<K> leftChild;
+        kdtNode<K> rightChild;
+
+        kdtNode(K point, int x, int y, int splitDirection) {
+            key = point;
+            this.splitDirection = splitDirection;
+            posX = x;
+            posY = y;
+            leftChild = null;
+            rightChild = null;
+        }
+
+        int getX() {
+            return posX;
+        }
+
+        int getY() {
+            return posY;
+        }
+
+        int getSplitDirection() {
+            return splitDirection;
+        }
+    }
+
+    KDTree() {}
+
+    public void put(K point, int x, int y) {
+        root = put(point, x, y, root, COORDINATE_X);
+    }
+
+    public boolean barrierInRange(int x, int y, int height, int width, Map<K, Engine.RectangleSize> roomMap) {
+        Stack<kdtNode<K>> traversalHelper = new Stack<>();
+        HashSet<kdtNode<K>> visitedRecord = new HashSet<>();
+        kdtNode<K> cur;
+        traversalHelper.push(root);
+        while (!traversalHelper.isEmpty()) {
+            cur = traversalHelper.peek();
+            if (cur == null) {
+                traversalHelper.pop();
+            } else {
+                int curX = cur.getX();
+                 int curY = cur.getY();
+                int offsetX = curX - x;
+                int offsetY = curY - y;
+
+                if (offsetX >= 0 && offsetX < width + 2 && offsetY >= 0 && offsetY < height + 2) { // current p drops within range.
+                    return true;
+                } else if (roomMap.containsKey(cur.key)) {
+                    int curWidth = roomMap.get(cur.key).width;
+                    int curHeight = roomMap.get(cur.key).height;
+                    int curOffsetX = -offsetX;
+                    int curOffsetY = -offsetY;
+                    if (curOffsetX >= 0 && curOffsetX < curWidth + 2 && curOffsetY >= 0 && curOffsetY < curHeight + 2) {
+                        return true;
+                    }
+                }
+                int goodSideFlagOffset = cur.getSplitDirection() == COORDINATE_X ?
+                        x - curX : y - curY; // 1, 2
+                int heightOrWidth = cur.getSplitDirection() == COORDINATE_X ?
+                        width : height;
+
+                if (visitedRecord.contains(cur)) { // 2 Second visit, bad side.
+                    traversalHelper.pop();
+                    kdtNode<K> badSide = goodSideFlagOffset + heightOrWidth + 2 <= 0 ? // bad side?
+                            cur.rightChild : cur.leftChild; // 2
+                    badSide = goodSideFlagOffset > 0 ? cur.leftChild : badSide;
+                    traversalHelper.push(badSide);
+                } else { // 1 First visit, good side.
+                    visitedRecord.add(cur);
+                    kdtNode<K> goodSide = goodSideFlagOffset + heightOrWidth + 2 <= 0 ? // good side ?
+                            cur.leftChild : cur.rightChild; // 1
+                    goodSide = goodSideFlagOffset > 0 ? cur.rightChild : goodSide;
+                    traversalHelper.push(goodSide);
+                }
+            }
+        }
+        return false;
+    }
+}
